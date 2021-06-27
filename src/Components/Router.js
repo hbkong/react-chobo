97d3030baa1290e4c3a288c7a39c3d0a5bf10977
Hyeon Bae Kong <carpedm43@gmail.com>
2021-06-28 00:56:51 +0900

    modify styled-components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {HashRouter, Route} from "react-router-dom";
+import {BrowserRouter, Redirect, Route} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "./Header";
 
 
 export default () => (
-    <HashRouter>
-        <>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
-        </>
-    </HashRouter>
+    <BrowserRouter>
+        <Header/>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/"></Redirect>
+    </BrowserRouter>
 )
